46346c7269c9fc8c21c7c6e3300acd659a333100
oeway <oeway007@gmail.com>
2014-04-12 07:02:35 +0000

    fix socket.io

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
 var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
-var io = require('socket.io')(server);
-var port = 80;
+var io = require('socket.io').listen(server,{log:true});
+var port = process.env.PORT || 80;
 
 server.listen(port, function () {
   console.log('Server listening at port %d', port);
