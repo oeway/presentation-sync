c88130405adacc6f06ab812d3a378b91ce8f56ee
oeway <oeway007@gmail.com>
2014-04-12 06:57:26 +0000

    fix index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
-var io = require('./socket.io')(server);
+var io = require('socket.io')(server);
 var port = 80;
 
 server.listen(port, function () {
